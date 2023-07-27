09bc43fb7f37f1272ca48d98ac2456380f5200db
Lucas Medeiros <lucas-medeiros@live.com>
2023-07-26 22:10:17 -0300

    fixed home component issues

--- frontend/src/app/home/home/home.module.ts
+++ frontend/src/app/home/home/home.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { HomeComponent } from './home.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './home.component';
 
 
 
 
 
 @NgModule({
-  // declarations: [
-  //   HomeComponent
-  // ],
   imports: [
     CommonModule,
     AppMaterialModule,
@@ -23,10 +20,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatCardModule,
     MatMenuModule,
+    MatIconModule
     
-  ],
-  // exports: [
-  //   HomeComponent
-  // ]
+  ]
 })
 export class HomeModule { }
